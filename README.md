f267578dc9a0449791aae7f8c8b09e2789222142
zhufengnodejs <zhufengnodejs@126.com>
2017-05-29 16:29:58 +0800

    4. 创建服务并显示保存的电影

--- README.md
+++ README.md
@@ -14,4 +14,5 @@ npm install request iconv-lite cheerio async cron debug mongoose express ejs -S
 1. 创建项目并编写读取方法
 2. 把得到的电影数组保存到数据库中
 3. 编写main方法实现爬虫
+4. 创建服务并显示保存的电影
 
--- tasks/main.js
+++ tasks/main.js
@@ -3,9 +3,13 @@ let write = require('./write').write;
 let async = require('async');
 let debug = require('debug')('crawl:main');
 let url = 'http://top.baidu.com/buzz?b=26&c=1&fr=topcategory_c1';
+let Movie = require('../model').Movie;
 function start(){
    async.waterfall([
        function(callback){
+        Movie.remove({},callback);
+       },
+       function(data,callback){
          read(url,callback);//callback(err,movies)
        },
        function(movies,callback){
